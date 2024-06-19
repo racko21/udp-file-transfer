575acd67f2fe18e992ab7066acef9e5734eed80f
racko21 <florianrack@gmx.at>
2024-06-20 00:42:16 +0200

    translated to Go

---
+++ recieve.go
@@ -0,0 +1,149 @@
+package main
+
+import (
+	"bytes"
+	"crypto/md5"
+	"encoding/binary"
+	"fmt"
+	"io"
+	"net"
+	"os"
+	"strconv"
+	"time"
+)
+
+const (
+	PAYLOAD_LENGTH = 1024
+	PACKET_SIZE    = PAYLOAD_LENGTH + 6
+	TIMEOUT        = 100 * time.Millisecond
+)
+
+func receiveFile(port int) {
+	// addr, err := net.ResolveUDPAddr("udp", fmt.Sprintf(":%d", port))
+	// if err != nil {
+	// 	panic(err)
+	// }
+	addr := net.UDPAddr{
+		IP:   net.ParseIP("0.0.0.0"),
+		Port: port,
+	}
+	conn, err := net.ListenUDP("udp", &addr)
+	if err != nil {
+		fmt.Printf("Listen err %v", err)
+	}
+	defer conn.Close()
+
+	buffer := make([]byte, PACKET_SIZE)
+	var transmissionID uint16
+	var sequenceNumber, maxSequenceNumber uint32
+	var fileName string
+	var TRANSMISSIONID uint16
+	receivedData := make(map[uint32][]byte)
+
+	fmt.Printf("Listening on port %d...\n", port)
+
+	conn.SetReadDeadline(time.Now().Add(10 * time.Second))
+	for {
+		n, raddr, err := conn.ReadFromUDP(buffer)
+		if err != nil {
+			fmt.Println("Timeout: file information not received")
+			return
+		}
+		if sequenceNumber == 0 {
+			TRANSMISSIONID = binary.BigEndian.Uint16(buffer[0:2])
+			sequenceNumber = binary.BigEndian.Uint32(buffer[2:6])
+			maxSequenceNumber = binary.BigEndian.Uint32(buffer[6:10])
+			fileName = string(buffer[10:n])
+			fmt.Printf("Receiving file: %s\n", fileName)
+			fmt.Printf("sequenceNumber: %v\n", sequenceNumber)
+			ackPacket := make([]byte, 6)
+			binary.BigEndian.PutUint16(ackPacket[0:2], transmissionID)
+			binary.BigEndian.PutUint32(ackPacket[2:6], sequenceNumber)
+			conn.WriteToUDP(ackPacket, raddr)
+			break
+		}
+	}
+
+	var fileMd5 []byte
+	conn.SetReadDeadline(time.Now().Add(TIMEOUT))
+	for {
+		n, raddr, err := conn.ReadFromUDP(buffer)
+		if err != nil {
+			if netErr, ok := err.(net.Error); ok && netErr.Timeout() {
+				conn.SetReadDeadline(time.Now().Add(TIMEOUT))
+				continue
+			}
+		}
+		transmissionID = binary.BigEndian.Uint16(buffer[0:2])
+		sequenceNumber = binary.BigEndian.Uint32(buffer[2:6])
+		fmt.Printf("sequenceNumber: %v\n", sequenceNumber)
+
+		if transmissionID != TRANSMISSIONID {
+			continue
+		}
+
+		if sequenceNumber == maxSequenceNumber {
+			fileMd5 = buffer[6:n]
+			ackPacket := make([]byte, 6)
+			binary.BigEndian.PutUint16(ackPacket[0:2], transmissionID)
+			binary.BigEndian.PutUint32(ackPacket[2:6], sequenceNumber)
+			conn.WriteToUDP(ackPacket, raddr)
+			break
+		} else {
+			receivedData[sequenceNumber] = make([]byte, n-6)
+			copy(receivedData[sequenceNumber], buffer[6:n])
+			ackPacket := make([]byte, 6)
+			binary.BigEndian.PutUint16(ackPacket[0:2], transmissionID)
+			binary.BigEndian.PutUint32(ackPacket[2:6], sequenceNumber)
+			conn.WriteToUDP(ackPacket, raddr)
+		}
+	}
+
+	file, err := os.Create(fileName)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	for i := uint32(1); i < maxSequenceNumber; i++ {
+		file.Write(receivedData[i])
+	}
+
+	verifyFileIntegrity(fileName, fileMd5)
+}
+
+func verifyFileIntegrity(filePath string, originalMd5 []byte) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	hash := md5.New()
+	if _, err := io.Copy(hash, file); err != nil {
+		panic(err)
+	}
+	md5 := hash.Sum(nil)
+	fmt.Println(md5)
+	fmt.Println(originalMd5)
+
+	if bytes.Equal(md5, originalMd5) {
+		fmt.Println("File integrity verified successfully.")
+	} else {
+		fmt.Println("File integrity verification failed.")
+	}
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: go run receive.go <port>")
+		os.Exit(1)
+	}
+
+	port, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		fmt.Println("Invalid port number")
+		os.Exit(1)
+	}
+	receiveFile(port)
+}
--- recieve.py
+++ recieve.py
@@ -1,76 +1,3 @@
-# import socket
-# from struct import pack, unpack
-# import hashlib
-# import sys
-# import time
-#
-# PAYLOAD_LENGTH = 1024
-# PACKET_SIZE = PAYLOAD_LENGTH + 16 + 32
-# WINDOW_SIZE = 8
-#
-# def receive_file(port):
-#     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-#     sock.bind(("", port))
-#     sock.settimeout(3)
-#
-#     file_name = ""
-#     max_sequence_number = float("inf")
-#     sequence_number = float("inf")
-#     ack = 1
-#     recieving = True
-#     received_data = {}
-#     window = []
-#     TRANSMISSION_ID = None
-#
-#     print(f"Listening on port {port}...")
-#
-#     # recieve file information
-#     start = time.time()
-#     while True:
-#         packet, addr = sock.recvfrom(PACKET_SIZE)
-#         transmission_id, sequence_number = unpack("!HI", packet[:6])
-#         timer = time.time()
-#         if timer - start > 10:
-#             print("Timeout file information not recieved")
-#         if sequence_number == 0:
-#             TRANSMISSION_ID = transmission_id
-#             ack_packet = pack("!Ic", ack, 0)
-#             sock.sendto(ack_packet, addr)
-#             break
-#
-#     while recieving == True:
-#         try:
-#             for i in range(WINDOW_SIZE):
-#         # packet, addr = sock.recvfrom(PACKET_SIZE)
-#         # transmission_id, sequence_number = unpack("!HI", packet[:6])
-#                 packet, addr = sock.recvfrom(PACKET_SIZE)
-#                 transmission_id, sequence_number = unpack("!HI", packet[:6])
-#                 if transmission_id == TRANSMISSION_ID:
-#                     received_data[sequence_number] = packet[6:]
-#                     if sequence_number - ack < WINDOW_SIZE:
-#                         window.append(sequence_number)
-#                         ack_packet = pack("!Ic", ack, 0)
-#                         sock.sendto(ack_packet, addr)
-#         except socket.timeout:
-#         #
-#         # # if TRANSMISSION_ID is None:
-#         # #     TRANSMISSION_ID = transmission_id
-#         #
-#         # if transmission_id == TRANSMISSION_ID:
-#         #     # if sequence_number == 0:
-#         #     #     max_sequence_number = unpack("!I", packet[6:10])[0]
-#         #     #     file_name = packet[10:].decode().strip()
-#         #     #     ack_packet = pack("!Ic", 1, "c")
-#         #     #     sock.sendto(ack_packet, addr)
-#         #     #     print(f"Receiving file: {file_name}")
-#         #     #     packet_count += 1
-#         #     if sequence_number == max_sequence_number:
-#         #         break
-#         #         # file_md5 = packet[6:]
-#         #         # print("MD5:", file_md5)
-#         #         # packet_count += 1
-#         #     else:
-
 import socket
 import struct
 import hashlib
@@ -86,8 +13,9 @@ def receive_file(port):
 
     received_data = {}
     sequence_number = 0
-    max_sequence_number = 0
-    transmission_id = None
+    max_sequence_number = float("inf")
+    packet_count = 0
+    TRANSMISSION_ID = None
     file_name = ""
 
     print(f"Listening on port {port}...")
@@ -97,8 +25,8 @@ def receive_file(port):
         transmission_id, sequence_number = struct.unpack("!HI", packet[:6])
 
         if transmission_id is None:
-            transmission_id = transmission_id
-        elif transmission_id != transmission_id:
+            TRANSMISSION_ID = transmission_id
+        elif transmission_id != TRANSMISSION_ID:
             continue
 
         if sequence_number == 0:
@@ -112,10 +40,9 @@ def receive_file(port):
         else:
             if sequence_number not in received_data:
                 received_data[sequence_number] = packet[6:]
-                sock.sendto(struct.pack("!HI", transmission_id, sequence_number), addr)
-
-    print(f"max_sequence_number: {max_sequence_number}")
-    print(f"last sequence_number: {sequence_number}")
+                packet_count += 1
+            sock.sendto(struct.pack("!HI", transmission_id, sequence_number), addr)
+            print(f"sequence_number: {sequence_number}")
 
     # Reconstruct the file
     if file_name == "":
@@ -140,6 +67,7 @@ def verify_file_integrity(file_path, original_md5):
         print("File integrity verified successfully.")
     else:
         print("File integrity verification failed.")
+        print(md5.hexdigest())
 
 
 if __name__ == "__main__":
